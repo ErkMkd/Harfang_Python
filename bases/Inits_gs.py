61784a45f8000165c8c1cec4f1d8a1dde455ec8a
EricKernin <contact@eric-kernin.com>
2017-09-27 18:01:37 +0200

    Update

--- bases/Inits_gs.py
+++ bases/Inits_gs.py
@@ -1,31 +1,35 @@
 # -*-coding:Utf-8 -*
 
+# ===========================================================
+
+#              - Harfang - www.harfang3d.com
+
+#                       - Python -
+
+#       Basic initialisations and use of main devices
+
+# ===========================================================
+
 import gs
 
-
-#------------ Inits --------------
-
-#gs.MountFileDriver(gs.StdFileDriver())
-#gs.LoadPlugins(gs.get_default_plugins_path())
-
-#------------ Display object and Output screen:
+# ------------ Display object and Output screen:
 
 renderer = gs.EglRenderer()
-renderer.Open(640,480)
+renderer.Open(640, 480)
 
-#------------ Get devices:
+# ------------ Check devices:
 
-print ("Installed devices:")
+print("Installed devices:")
 devices = gs.GetInputSystem().GetDevices()
 for device in devices:
-    print("Id: "+str(device.GetId()) + " - Name : " + device.GetName())
+    print("Id: " + str(device.GetId()) + " - Name : " + device.GetName())
+
+# ------------ Get devices:
 
 keyboard_device = gs.GetInputSystem().GetDevice("keyboard")
 mouse_device = gs.GetInputSystem().GetDevice("mouse")
 
-
-
-#------------ Main loop:
+# ------------ Main loop:
 
 while renderer.GetDefaultOutputWindow():
 
@@ -40,31 +44,32 @@ while renderer.GetDefaultOutputWindow():
         print("A")
 
     elif keyboard_device.IsDown(gs.InputDevice.KeyZ):
-        print ("Z")
+        print("Z")
 
     elif keyboard_device.WasDown(gs.InputDevice.KeyE):
         print("E")
 
     # --- Keyboard key code:
     for key in range(0, gs.InputDevice.KeyLast):
-            if keyboard_device.WasPressed(key):
-                print("Key code: %d" % key)
-
+        if keyboard_device.WasPressed(key):
+            print("Key code: %d" % key)
 
     # --- Mouse controls:
 
     if mouse_device.WasButtonPressed(gs.InputDevice.Button0):
-        print ("Mouse position: %d,%d" % (mouse_device.GetValue(gs.InputDevice.InputAxisX),mouse_device.GetValue(gs.InputDevice.InputAxisY)))
+        print("Mouse position: %d,%d" % (
+            mouse_device.GetValue(gs.InputDevice.InputAxisX), mouse_device.GetValue(gs.InputDevice.InputAxisY)))
 
     if mouse_device.IsButtonDown(gs.InputDevice.Button1):
-        print ("Mouse position: %d,%d" % (mouse_device.GetValue(gs.InputDevice.InputAxisX),mouse_device.GetValue(gs.InputDevice.InputAxisY)))
+        print("Mouse position: %d,%d" % (
+            mouse_device.GetValue(gs.InputDevice.InputAxisX), mouse_device.GetValue(gs.InputDevice.InputAxisY)))
 
     if mouse_device.WasButtonReleased(gs.InputDevice.Button2):
-        print ("mouse position: %d,%d" % (mouse_device.GetValue(gs.InputDevice.InputAxisX),mouse_device.GetValue(gs.InputDevice.InputAxisY)))
+        print("mouse position: %d,%d" % (
+            mouse_device.GetValue(gs.InputDevice.InputAxisX), mouse_device.GetValue(gs.InputDevice.InputAxisY)))
 
     # --- End of frame:
 
     renderer.DrawFrame()
     renderer.ShowFrame()
     renderer.UpdateOutputWindow()
-
